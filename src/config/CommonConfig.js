33eb655f03127ed600b475b0ae840eb54dbd31dd
Lê Đức Thắng <leducthang@Les-MacBook-Pro.local>
2020-10-08 00:42:36 +0700

    note default database

--- src/config/CommonConfig.js
+++ src/config/CommonConfig.js
@@ -1,4 +1,4 @@
 export default {
     PORT: process.env.SERVER_PORT || 3000,
-    DATABASE_URL: process.env.DATABASE_URL //change in .env
+    DATABASE_URL: process.env.DATABASE_URL // current database: edukite
 }
